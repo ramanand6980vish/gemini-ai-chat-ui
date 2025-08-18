4dc514fb07ecae3b1fff1783ecc4c9f77c47da49
ramanand6980vish <ramanand.vi2011@gmail.com>
2025-08-18 22:39:29 +0530

    Update constants.jsx

--- constants.jsx
+++ constants.jsx
@@ -1,5 +1,5 @@
 
 
-// export const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAkQTGOr3Rifdfv-jPWxpHGxGpdaBbRwPY";
 
-export const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCxItwEUFGwyhnINlBQSSZZa5r8QiEb5Dw"
+
+export const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCxItwEUFG...............api_key"
